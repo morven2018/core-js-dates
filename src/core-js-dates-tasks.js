9e6be6973fb262e5072a6b9b77a15624e808ab38
morven2018 <you@example.com>
2025-02-28 23:15:05 +0300

    fix: update getTime function

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -31,9 +31,8 @@ function dateToTimestamp(date) {
  * Date(2023, 5, 1, 8, 20, 55) => '08:20:55'
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
-function getTime(/* date */) {
-  throw new Error('Not implemented');
-  // return date.toLocaleTimeString();
+function getTime(date) {
+  return `${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}:${String(date.getSeconds()).padStart(2, '0')}`;
 }
 
 /**
