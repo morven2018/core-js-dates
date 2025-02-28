6a935a598305573db5aa9cf0b5b05c6c75c29b4e
morven2018 <you@example.com>
2025-03-01 00:10:05 +0300

    feat:  add quoter solution

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -244,8 +244,23 @@ function getNextFridayThe13th(date) {
  * Date(2024, 5, 1) => 2
  * Date(2024, 10, 10) => 4
  */
-function getQuarter(/* date */) {
-  throw new Error('Not implemented');
+function getQuarter(date) {
+  switch (date.getMonth()) {
+    case 0:
+    case 1:
+    case 2:
+      return 1;
+    case 3:
+    case 4:
+    case 5:
+      return 2;
+    case 6:
+    case 7:
+    case 8:
+      return 3;
+    default:
+      return 4;
+  }
 }
 
 /**
